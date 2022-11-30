6f35fedb93c3e63a1124a031cee5fa8e60be4b23
zbc <1511828529@qq.com>
2022-11-30 11:25:32 +0800

    v0.1.5

--- tools/dataExec.js
+++ tools/dataExec.js
@@ -29,7 +29,7 @@ function _deepClone(obj) {
 }
 
 // 深克隆
-m.deepClone = _deepClone
+m.deepClone = _deepClone;
 
 // 将数字西方化显示（千位加逗号）
 m.commaedNumber = x => {
@@ -42,19 +42,43 @@ m.commaedNumber = x => {
     } else {
       num = x;
     }
-    let numStr = num.toString();
-    let strArr = numStr.split("");
-    let rArr = strArr.reverse();
-    const L = strArr.length;
-    let resArr = [];
-    for (let i = 0; i < L; i++) {
-      resArr.push(rArr[i]);
-      if (!(i % 2) && i !== 0) {
-        if (rArr[i + 1]) resArr.push(",");
-      }
-    }
 
-    return resArr.reverse().join("");
+    // 先判断是否有小数部分
+    let withXS = Math.trunc(num) < num;
+    // 对小数进行隔离处理
+
+    if (withXS) {
+      let zs = Math.trunc(num);
+      let xs = parseInt(`${num}`.split(".")[1]);
+
+      let numStr = zs.toString();
+      let strArr = numStr.split("");
+      let rArr = strArr.reverse();
+      const L = strArr.length;
+      let resArr = [];
+      for (let i = 0; i < L; i++) {
+        resArr.push(rArr[i]);
+        if (!(i % 2) && i !== 0) {
+          if (rArr[i + 1]) resArr.push(",");
+        }
+      }
+
+      return resArr.reverse().join("") + `.${xs}`;
+    } else {
+      let numStr = num.toString();
+      let strArr = numStr.split("");
+      let rArr = strArr.reverse();
+      const L = strArr.length;
+      let resArr = [];
+      for (let i = 0; i < L; i++) {
+        resArr.push(rArr[i]);
+        if (!(i % 2) && i !== 0) {
+          if (rArr[i + 1]) resArr.push(",");
+        }
+      }
+
+      return resArr.reverse().join("");
+    }
   }
 };
 
--- tools/dataGen.js
+++ tools/dataGen.js
@@ -29,33 +29,71 @@ m.genRandomNumArr = (min, max, num, fixed = 0) => {
 // 生成一个以某天为起始，之前n天的字符串数组
 // YYYY-MM-DD
 m.genDatesArr = (date, n) => {
-  let result = [];
-  // moment
-  if (date._isAMomentObject) {
-    for (let i = 0; i < n; i++) {
-      result.unshift(date.subtract(1, "days").format("YYYY-MM-DD"));
-    }
-  } else {
-    throw new Error("genDatesArr函数中参数date不是一个moment对象或者moment版本过低");
-  }
+	let result = [];
+	// moment
+	if (date._isAMomentObject) {
+		for (let i = 0; i < n; i++) {
+			result.unshift(date.subtract(1, "days").format("YYYY-MM-DD"));
+		}
+	} else {
+		throw new Error(
+			"genDatesArr函数中参数date不是一个moment对象或者moment版本过低"
+		);
+	}
 
-  return result;
+	return result;
 };
 
 // 生成一个以某月为起始，之前n个月的字符串数组
 // YYYY-MM
 m.genMonthsArr = (month, n) => {
-  let result = [];
-  // moment
-  if (month._isAMomentObject) {
-    for (let i = 0; i < n; i++) {
-      result.unshift(month.subtract(1, "months").format("YYYY-MM"));
-    }
-  } else {
-    throw new Error("genDatesArr函数中参数date不是一个moment对象");
-  }
+	let result = [];
+	// moment
+	if (month._isAMomentObject) {
+		for (let i = 0; i < n; i++) {
+			result.unshift(month.subtract(1, "months").format("YYYY-MM"));
+		}
+	} else {
+		throw new Error("genDatesArr函数中参数date不是一个moment对象");
+	}
 
-  return result;
+	return result;
+};
+
+// 生成数字序列
+m.genNumberStream = (start = 1, length = 10, spliter = 1) => {
+	let arr = [];
+	for (let i = 0; i < length; i++) {
+		arr.push(start + i * spliter);
+	}
+	return arr;
+};
+
+// 生成随机编码
+m.genRandomCode = (
+	length,
+	option = { onlyChar: false, onlysChar: false, withChar: false }
+) => {
+	const { onlyChar, onlysChar, withChar } = option;
+	// ascii 码表对照数字索引
+	const numList = m.genNumberStream(48, 10);
+	const wordList = m.genNumberStream(65, 26);
+	const wordList2 = m.genNumberStream(97, 26);
+
+	const targetList = onlyChar
+		? wordList
+		: onlysChar
+		? wordList2
+		: withChar
+		? [...numList, ...wordList, ...wordList2]
+		: numList;
+
+	let resStr = "";
+	for (let i = 0; i < length; i++) {
+		let index = parseInt(Math.random() * 10);
+		resStr += String.fromCharCode(targetList[index]);
+	}
+	return resStr;
 };
 
 export default m;
